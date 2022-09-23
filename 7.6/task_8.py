8d55b58667353afd9d670b4e74d8431ab139e2b0
vlad1sat <pyatkov200316@mail.ru>
2022-09-23 12:22:54 +0500

    change 6.7 task_8

--- 7.6/task_8.py
+++ 7.6/task_8.py
@@ -1,6 +1,6 @@
 for number in range(10, 100):
     sum_number = 1
-    for char in str(number):
-        sum_number *= int(char)
+    for symbol in str(number):
+        sum_number *= int(symbol)
     if sum_number * 3 == number:
         print(number)
