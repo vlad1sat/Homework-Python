102ad37471ed5d41b940fd94a463259877ad1146
vlad1sat <pyatkov200316@mail.ru>
2022-10-01 23:14:04 +0500

    add missing program

--- 8.6/task_10.py
+++ 8.6/task_10.py
@@ -1,3 +1,18 @@
-boys = int(input("Введите количество мальчиков: "))
-girls = int(input("Введите количество девочек: "))
-
+count_boys = int(input("Введите количество мальчиков: "))
+count_girls = int(input("Введите количество девочек: "))
+result = ""
+if (count_boys > 2 * count_girls) or (count_girls > 2 * count_boys):
+    print("Нет решения.")
+elif count_boys >= count_girls:
+    difference = count_boys - count_girls
+    for bgb in range(difference):
+        result += "BGB"
+    for bg in range(count_girls - difference):
+        result += "BG"
+else:
+    difference = count_girls - count_boys
+    for gbg in range(difference):
+        result += "GBG"
+    for gb in range(count_boys - difference):
+        result += "GB"
+print(result)
