966d803a81be62db7da640b0a1e227c0b638e660
vlad1sat <pyatkov200316@mail.ru>
2022-10-27 00:38:16 +0500

    change 20.8.9

--- 20.8/task_9.py
+++ 20.8/task_9.py
@@ -1,7 +1,3 @@
-def score_key(score):
-    return score[1][0] - score[1][1]
-
-
 score_table = {}
 count_protocols = int(input("Общее количество строк протокола: "))
 print("Записи (результат и имя):")
@@ -16,7 +12,7 @@ for time in range(count_protocols):
         score_table[name] = [result, time]
 
 scores = list(score_table.items())
-scores.sort(key=score_key, reverse=True)
+scores.sort(key=scores[1][0] - scores[1][1], reverse=True)
 print("\nИтоги соревнований:")
 for winner_index in range(2):
     print(f"{winner_index + 1} место. {scores[winner_index][0]} {scores[winner_index][1][0]}")
