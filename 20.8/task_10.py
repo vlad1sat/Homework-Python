3882841aae426b74bb4618cc547c6cd67b30ecd6
vlad1sat <pyatkov200316@mail.ru>
2022-11-02 21:10:37 +0500

    update 20.8.10

--- 20.8/task_10.py
+++ 20.8/task_10.py
@@ -1,12 +1,11 @@
 def my_zip(symbols, numbers):
-    zip_dict = dict()
-    for index in range(min(len(symbols), len(numbers))):
-        zip_dict[symbols[index]] = numbers[index]
-    for element_zip in zip_dict.items():
-        print(element_zip)
+    return ((symbols[index], numbers[index]) for index in range(min(len(symbols), len(numbers))))
 
 
 user_str = input("Строка: ")
 user_symbols = list(user_str)
 user_numbers = [int(number) for number in tuple(input("Кортеж чисел:(через запятую): ").split(', '))]
-my_zip(user_symbols, user_numbers)
+zip_user = my_zip(user_symbols, user_numbers)
+print(zip_user)
+for element_zip in zip_user:
+    print(element_zip)
